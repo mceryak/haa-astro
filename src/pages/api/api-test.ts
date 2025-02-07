0058f7c9212049a4dd255ef1fabd9d7cb23e8b8d
Michael Ceryak <mceryak@yahoo.com>
2025-02-07 11:43:39 -0600

    trying hybrid

--- src/pages/api/api-test.ts
+++ src/pages/api/api-test.ts
@@ -1,13 +1,13 @@
-import type { APIContext } from "astro";
+// import type { APIContext } from "astro";
 
-export async function GET({ locals, params, request }: APIContext) {
-  const { HAA_KV } = locals.runtime.env;
-  const value = await HAA_KV.get('test');
-  return new Response(
-    JSON.stringify({
-      name: 'Astro',
-      url: 'https://astro.build/',
-      value
-    })
-  )
-}
+// export async function GET({ locals, params, request }: APIContext) {
+//   const { HAA_KV } = locals.runtime.env;
+//   const value = await HAA_KV.get('test');
+//   return new Response(
+//     JSON.stringify({
+//       name: 'Astro',
+//       url: 'https://astro.build/',
+//       value
+//     })
+//   )
+// }
