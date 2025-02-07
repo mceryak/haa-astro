98a6aab3a68336da4519bf537ebd0bdac3d3f619
Michael Ceryak <mceryak@yahoo.com>
2025-02-07 11:47:41 -0600

    try static with prerender false

--- src/pages/api/api-test.ts
+++ src/pages/api/api-test.ts
@@ -1,13 +1,15 @@
-// import type { APIContext } from "astro";
+import type { APIContext } from "astro";
 
-// export async function GET({ locals, params, request }: APIContext) {
-//   const { HAA_KV } = locals.runtime.env;
-//   const value = await HAA_KV.get('test');
-//   return new Response(
-//     JSON.stringify({
-//       name: 'Astro',
-//       url: 'https://astro.build/',
-//       value
-//     })
-//   )
-// }
+export const prerender = false;
+
+export async function GET({ locals, params, request }: APIContext) {
+  const { HAA_KV } = locals.runtime.env;
+  const value = await HAA_KV.get('test');
+  return new Response(
+    JSON.stringify({
+      name: 'Astro',
+      url: 'https://astro.build/',
+      value
+    })
+  )
+}
