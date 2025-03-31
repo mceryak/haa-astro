a852facee8056d8ab25bd599f27b8318588dcfe2
Michael Ceryak <mceryak@yahoo.com>
2025-03-31 13:29:31 -0500

    try again

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 export const { signIn, signUp, getSession, useSession, listSessions, signOut } = createAuthClient({
   baseURL: "https://michaelceryak.com"
+  // baseURL: "http://localhost:4321"
+  // baseURL: "http://localhost:8788"
 });
 
 export type SocialProvider = "github" | "apple" | "discord" | "facebook" | "microsoft" | "google" | "spotify" | "twitch" | "twitter" | "dropbox" | "linkedin" | "gitlab" | "reddit";
