c3f18809e58dca79fc8c42859dcec931695407bf
Michael Ceryak <mceryak@yahoo.com>
2025-04-24 13:59:45 -0500

    updated preview commands

--- deploy.js
+++ deploy.js
@@ -1,5 +1,5 @@
 import express, { json } from 'express';
-import { exec } from "child_process";
+import { exec, spawn } from "child_process";
 
 const app = express();
 const PORT = 8888; // The port your local listener will run on
@@ -7,19 +7,56 @@ const PORT = 8888; // The port your local listener will run on
 // Middleware to parse JSON bodies (if needed)
 app.use(json());
 
+let previewProcess;
 app.get("/preview", (req, res) => {
   console.log('Preview Request Received.');
-  
+
   exec("./preview.sh", (err, stdout, stderr) => {
   // exec("preview.cmd", (err, stdout, stderr) => {
       if (err) {
         console.error("Preview failed:", stderr);
         return res.status(500).json({ success: false, output: stdout, error: stderr }) //.send(`Error: ${stderr}`);
       }
-      console.log("Preview up and running:", stdout);
-      res.json({ success: true, output: stdout, error: null }) //.send("Deployment completed!");
+      console.log("Ready to start preview. ", stdout);
+      previewProcess = spawn('npm', ['run', 'preview'], {
+        stdio: 'pipe',
+        shell: true,
+      });
+      previewProcess.stderr.on('data', (data) => {
+        console.error('Error loading preview: ' + data.toString());
+      });
+      previewProcess.stdout.on('data', (data) => {
+        const text = data.toString();
+        console.log('[stdout]', text);
+
+        // Example: look for "Local" URL
+        if (text.includes('localhost:')) {
+          const errorMatch = text.match(/Address already in use \(.*\)/);
+          const match = text.match(/http:\/\/localhost:\d+/);
+          if (errorMatch) {
+            console.log(errorMatch[0])
+          }
+          if (match) {
+            console.log('Server started at:', match[0]);
+            // res.json({ success: true, serverRunningAtUrl: match[0] });
+            res.redirect(match[0]);
+          }
+        }
+      });
     });
 });
+
+function shutdown() {
+  console.log('\n🛑 Shutting down Express and preview server...');
+  if (previewProcess) {
+    previewProcess.kill('SIGTERM'); // or 'SIGINT'
+  }
+  process.exit(0);
+}
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
+
 
 app.get("/deploy", (req, res) => {
   console.log("Deployment request received!");
---
+++ preview.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Starting preview..."
+git pull origin main
+echo "Successfully cloned repo."
+npm install
+echo "Successfully installed dependencies."
